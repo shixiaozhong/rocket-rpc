cadc78a0721a55fb2a1b4719e4205d1795722d99
shixiaozhong <257988550@qq.com>
2023-10-12 23:17:27 +0800

    finish log and config and mutex

---
+++ testcases/test_log.cc
@@ -0,0 +1,32 @@
+#include "rocket/common/config.h"
+#include "rocket/common/log.h"
+#include <pthread.h>
+
+void *fun(void *) {
+  int i = 20;
+  while (i--) {
+    DEBUGLOG("debug this is thread in %s", "fun");
+    INFOLOG("info this is thread in %s", "fun");
+  }
+
+  return NULL;
+}
+
+int main() {
+
+  rocket::Config::SetGlobalConfig("../conf/rocket.xml");
+
+  rocket::Logger::InitGlobalLogger();
+
+  pthread_t thread;
+  pthread_create(&thread, NULL, &fun, NULL);
+
+  int i = 20;
+  while (i--) {
+    DEBUGLOG("test debug log %s", "11");
+    INFOLOG("test info log %s", "11");
+  }
+
+  pthread_join(thread, NULL);
+  return 0;
+}
